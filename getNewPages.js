14313b0b3cc2fc694c90481d01ccf88b67372083
Jasper Wong <wlcjasper@gmail.com>
2024-07-07 21:52:27 +0100

    Use ?. to handle cases where there is no property

--- getNewPages.js
+++ getNewPages.js
@@ -8,22 +8,34 @@ const puppeteer = require("puppeteer")
     "https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes"
   )
   const pageEntries = await page.evaluate(() => {
-    const table = document.querySelector(
+    const indexTable = document.querySelector(
       "#content > article > section:nth-child(3) > div > figure > table"
     )
-    const allAnchors = table.querySelectorAll("a")
+    // There are three <tr>s to a row
+    const allRows = indexTable.querySelectorAll("tr")
+    const chunkRows = []
+    for (var i = 0; i < allRows.length; i += 3) {
+      chunkRows.push([allRows[i], allRows[i + 1], allRows[i + 2]])
+    }
 
-    // Create an array to store the innerHTML of each anchor
-    const anchorContents = Array.from(allAnchors).map(
-      (anchor) => anchor.innerHTML
-    )
-
-    return anchorContents
+    // Map these chunked rows into a row object
+    const pageEntries = []
+    chunkRows.forEach((row) => {
+      const a = row[0].querySelector("a")
+      const page = {
+        link: `<a class="page-link "href="${a?.href}">${a?.innerText}</a>`,
+        text: `<div class="page-text">${
+          row[1]?.querySelector("td")?.innerText
+        }</div>`,
+      }
+      pageEntries.push(page)
     })
+    return pageEntries
+  })
 
-    // Save page objects to JSON on disk
+  // Save page objects to JSON on disk
 
-  const pageJSON = JSON.stringify({ content: `${pageEntries}` })
+  const pageJSON = JSON.stringify(pageEntries)
   fs.writeFile("./data/test.json", pageJSON, function (err) {
     if (err) {
       return console.log(err)
