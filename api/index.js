e5d5c02584ec8188bbfb0e1ca2910edab8925ca8
Jasper Wong <wlcjasper@gmail.com>
2024-07-08 10:16:14 +0100

    Wire up the node to the in

--- api/index.js
+++ api/index.js
@@ -1,20 +1,26 @@
-require('dotenv').config()
+require("dotenv").config()
 
 // init project
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+const express = require("express")
+const app = express()
+app.use(express.static("public"))
+app.set("view engine", "ejs")
 
-const pages = require('./data/pages.json');
-const rndPage = () => pages[Math.floor(Math.random() * pages.length)];
+const pages = require("./data/pages.json")
+const getRndPage = () => pages[Math.floor(Math.random() * pages.length)]
 
-app.get('/', (req, res) => res.render(__dirname + '/views/index'));
+app.get("/", (req, res) =>
+  res.render(__dirname + "/views/index", {
+    AttributeName: `${getRndPage().AttributeName}`,
+    Elements: `${getRndPage().Elements}`,
+    Description: `${getRndPage().Description}`,
+  })
+)
 
 // listen for requests :)
 
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+const listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port)
+})
 
 module.exports = app
